25b1e16d02cb839b772c910a9e19acbfa4c5e381
minhuw <wangmh15@gmail.com>
2023-09-14 22:09:21 +0800

    remove installation scripts

--- dot_scripts/executable_install_fish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-if [[ $OSTYPE == 'linux-gnu' ]]; then
-    if [[ $(lsb_release -i -s) == 'Ubuntu' ]]; then
-        sudo apt-add-repository ppa:fish-shell/release-3
-        sudo apt update
-        sudo apt install fish
-    fi
-fi
