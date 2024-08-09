894a8471c6882f864bf42e64c9ae41a38f37b220
minhuw <wangmh15@gmail.com>
2024-08-09 16:50:30 +0800

    feat: update wezterm configuration

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -8,13 +8,15 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("Cascadia Code")
 config.font_size = 13
 
-config.color_scheme = 'Material (base16)'
+config.color_scheme = 'PencilDark'
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true 
+config.tab_bar_at_bottom = true
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.75
+config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
+config.native_macos_fullscreen_mode = true
 
 -- and finally, return the configuration to wezterm
 return config
