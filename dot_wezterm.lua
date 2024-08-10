86079422f92d78f3554162ab3653358ab8d3fc42
minhuw <wangmh15@gmail.com>
2024-08-11 00:16:06 +0800

    feat: update wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -13,7 +13,6 @@ config.color_scheme = 'PencilDark'
 config.enable_tab_bar = true 
 config.tab_bar_at_bottom = true
 
-config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
 config.native_macos_fullscreen_mode = true
