d6ac838c06a3765251d8c57c9c9fec77209a434a
minhuw <wangmh15@gmail.com>
2026-08-16 16:25:03 +0800

    feat(fish): become the login shell and carry over PATH
    Source brew, ~/.local/bin, and the old zsh aliases so dropping
    zsh/p10k does not lose the daily environment.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,10 +1,33 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+if test -x /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
+else if test -x /usr/local/bin/brew
+    eval (/usr/local/bin/brew shellenv)
 end
 
-set EDITOR nvim
+fish_add_path -g $HOME/.local/bin
 
-starship init fish | source
-atuin init fish | source
+if test -d $HOME/.antigravity/antigravity/bin
+    fish_add_path -g $HOME/.antigravity/antigravity/bin
+end
 
+if test -d $HOME/.atrium/bin
+    fish_add_path -g $HOME/.atrium/bin
+end
 
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+if status is-interactive
+    if command -q starship
+        starship init fish | source
+    end
+    if command -q atuin
+        atuin init fish | source
+    end
+
+    if test (uname) = Darwin
+        command -q bat; and alias cat bat
+        command -q eza; and alias ls eza
+        command -q rg; and alias grep rg
+    end
+end
